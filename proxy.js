977536b5ba977b298ebcc4a8a83007b82281502d
oobcecada <oobcecada@gmail.com>
2025-01-28 19:20:31 -0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware para CORS
 app.use((req, res, next) => {
